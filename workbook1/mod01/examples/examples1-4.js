25192ea6522cea8ba4f1c060c8f2fdf84c6afdce
brightboost <info@brightboost.nl>
2022-07-26 20:06:28 +0200

    initial commit for learntocode structure

---
+++ workbook1/mod01/examples/examples1-4.js
@@ -0,0 +1,15 @@
+"use strict";
+
+var name = "123";
+name = "Miguel";
+console.log("Hi " + name);
+console.log("Okay bye!")
+
+// let's try to add a number to a string
+var duckSound = "Meow";
+var nr = 42;
+var result = duckSound + nr;
+console.log("the type is: " + typeof result);
+
+var briansAge = 32;
+console.log("Brian is " + (briansAge + 1))
