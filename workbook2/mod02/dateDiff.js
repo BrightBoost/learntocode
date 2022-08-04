e7be8022c404a12f5eeec0c8fa84fcfa0000ab49
brightboost <info@brightboost.nl>
2022-08-04 19:11:01 +0200

    playing around with getmonth

--- workbook2/mod02/dateDiff.js
+++ workbook2/mod02/dateDiff.js
@@ -1,4 +1,8 @@
 let dateOfBirth = new Date(1961, 5, 9);
+dateOfBirth.getMonth(); //0-11
+if(dateOfBirth.getMonth() == 5) {
+  console.log("june!" );
+}
 let now = new Date();
 
 let msDateOfBirth = dateOfBirth.getTime();
